9874804fbcb9a23a654a1d6ba45e77f99e5a2b16
bmxitalia <tommasocarraro96@gmail.com>
2025-04-15 10:02:43 +0200

    recsys function calling is working

--- app_function.py
+++ app_function.py
@@ -8,50 +8,246 @@ import chainlit as cl
 from data.my_functions.functions import RECOMMENDATION, METADATA
 from projectdavid_common.schemas.tools import ToolFunction
 import json
+from recbole.quick_start import load_data_and_model
+from recbole.utils.case_study import full_sort_topk, full_sort_scores
+import sqlite3
+import torch
 
-client = Entity(base_url="http://localhost:9000", api_key=os.getenv("API_KEY"))
-user = client.users.create_user(name='chainlit_user')
-thread = client.threads.create_thread(participant_ids=[user.id])
-assistant = client.assistants.create_assistant(instructions="You are a helpful AI assistant, connected to a movie database. This database will allow you to provide recommendations (for example top k movies for a specific user of the platform) or item metadata (for example title, director, and/or description) when requested by the user. If you detect the user asks for this kind of information, you will use function calling to retrieve that, otherwise (if the user does not specifically ask for recommendations or item metadata) you can reply normally. When generating JSONs, just generate them without additional contextual text.")  # , connected to a movie database. This database will allow you to provide recommendations or item metadata when requested by the user. If the user does not request this information, you can just reply normally.")
+# todo look for item metadata with multiple IDs instead of doing multiple searches in the table
+# todo create a summary of a user based on its past interactions, depict the user based on his/her past interactions
 
-try:
-    tool_func_obj_rec = ToolFunction(function=RECOMMENDATION['function'])
+config, model, dataset, train_data, valid_data, test_data = load_data_and_model(
+    model_file='./data/recsys/ml-100k/model.pth',
+)
 
-    tool_rec = client.tools.create_tool(
-        name=RECOMMENDATION['function']['name'],
-        type="function",
-        function=tool_func_obj_rec
-    )
+def create_db():
+    conn = sqlite3.connect('movies.db')
+    cursor = conn.cursor()
 
-    client.tools.associate_tool_with_assistant(tool_id=tool_rec.id, assistant_id=assistant.id)
+    cursor.execute('''CREATE TABLE IF NOT EXISTS movies_ml (item_id INTEGER PRIMARY KEY, title TEXT, release_date INTEGER, genres TEXT)''')
 
-    tool_func_obj_meta = ToolFunction(function=METADATA['function'])
+    # load data
+    with open('./data/recsys/ml-100k/ml-100k.item', 'r', encoding='utf-8') as f:
+        first_line = True
+        for line in f:
+            if first_line:
+                first_line = False
+                continue
+            parts = line.strip().split('\t')
+            if len(parts) < 4:
+                continue  # skip lines with missing data
 
-    tool_meta = client.tools.create_tool(
-        name=RECOMMENDATION['function']['name'],
-        type="function",
-        function=tool_func_obj_rec
-    )
+            item_id = int(parts[0])
+            movie_title = parts[1]
+            release_year = int(parts[2]) if parts[2].isdigit() else None
+            movie_class = parts[3]
 
-    client.tools.associate_tool_with_assistant(tool_id=tool_meta.id, assistant_id=assistant.id)
+            # Insert into the table
+            cursor.execute('INSERT OR IGNORE INTO movies_ml VALUES (?, ?, ?, ?)',
+                           (item_id, movie_title, release_year, movie_class))
 
-except ValueError as e:
-     print(f"ERROR: {e}")
-except Exception as e:
-     print(f"An unexpected error occurred: {e}")
+    cursor.execute('''CREATE TABLE IF NOT EXISTS interactions (user_id INTEGER PRIMARY KEY, items TEXT)''')
+
+    # Read the file and build the dictionary
+    user_interactions = {}
+    with open('./data/recsys/ml-100k/ml-100k.inter', 'r') as f:
+        first_line = True
+        for line in f:
+            if first_line:
+                first_line = False
+                continue
+            user_id, item_id, rating, timestamp = line.strip().split('\t')
+            user_id = int(user_id)
+            item_id = int(item_id)
+
+            if user_id not in user_interactions:
+                user_interactions[user_id] = []
+            user_interactions[user_id].append(item_id)
+
+    # Insert data into the table
+    for user_id, items in user_interactions.items():
+        items_str = ','.join(map(str, items))
+        cursor.execute('INSERT OR REPLACE INTO interactions (user_id, items) VALUES (?, ?)',
+                       (user_id, items_str))
+
+    conn.commit()
+    conn.close()
+
+def associate_tool_to_assistant(assistant_id, tool):
+    # check if tool is already associated
+    tools = client.tools.list_tools(assistant_id)
+    if tool['function']['name'] in [t['name'] for t in tools]:
+        print("Tool already associated!")
+    else:
+        try:
+            print("Associating tool to assistant")
+            tool_func_obj = ToolFunction(function=tool['function'])
+
+            tool_ = client.tools.create_tool(
+                name=tool['function']['name'],
+                type="function",
+                function=tool_func_obj
+            )
+
+            client.tools.associate_tool_with_assistant(tool_id=tool_.id,
+                                                       assistant_id=assistant_id, )
+
+        except ValueError as e:
+            print(f"ERROR: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+
+
+def define_query(filters):
+    query_parts = []
+    if 'genres' in filters:
+        genres = filters['genres']
+        for genre in genres:
+            query_parts.append(f"LOWER(genres) LIKE '%{genre}%'")
+
+    if 'release_date' in filters:
+        release_date = filters['release_date']
+        request = None
+        if isinstance(release_date, dict):
+            request = release_date['request']
+            release_date = release_date['threshold']
+        if request is not None:
+            query_parts.append(f"release_date > {release_date}") if request == "higher" else query_parts.append(f"release_date < {release_date}")
+        else:
+            query_parts.append(f"release_date = {release_date}")
+
+    return f"SELECT item_id FROM movies_ml WHERE {'AND '.join(query_parts)}"
+
+
+def get_interacted_items(user):
+    conn = sqlite3.connect('movies.db')
+    cursor = conn.cursor()
+
+    sql_query = f'SELECT items FROM interactions WHERE user_id = {user}'
+
+    cursor.execute(sql_query)
+    result = cursor.fetchone()
+
+    conn.close()
+
+    if result:
+        return result[0]
+    else:
+        return None
+
+
+def get_top_k_recommendations(params):
+    if 'user' in params and 'k' in params:
+        user = params.get('user')
+        k = params.get('k')
+        uid_series = dataset.token2id(dataset.uid_field, [str(user)])
+        # satisfy filters
+        if 'filters' in params:
+            filters = params.get('filters')
+            # execute sql query based on the filters to get items that satisfy the filters
+            sql_query = define_query(filters)
+            conn = sqlite3.connect('movies.db')
+            cursor = conn.cursor()
+            cursor.execute(sql_query)
+            result = cursor.fetchall()
+            conn.close()
+            # invoke the recommender system to get rating of all items satisfying the given conditions
+            item_ids = [str(row[0]) for row in result]
+            all_scores = full_sort_scores(uid_series, model, test_data, device=config['device'])
+            satisfying_item_scores = all_scores[0, dataset.token2id(dataset.iid_field, item_ids)]
+            _, sorted_indices = torch.sort(satisfying_item_scores, descending=True)
+            external_item_list = [item_ids[i] for i in sorted_indices[:k].cpu().numpy()]
+        else:
+            topk_score, topk_iid_list = full_sort_topk(uid_series, model, test_data, k=k,
+                                                       device=config['device'])
+            external_item_list = dataset.id2token(dataset.iid_field, topk_iid_list.cpu())[0]
+
+        print(external_item_list)
+        response_dict = {}
+        for item_id in external_item_list:
+            response_dict[item_id] = get_item_metadata(params={'item': int(item_id), 'specification': ["title", "release_date", "genres"]}, return_dict=True)
+
+        # get items interacted by user ID
+        item_ids = get_interacted_items(user)
+        # get metadata of interacted items
+        interaction_dict = {}
+        for item_id in item_ids.split(','):
+            interaction_dict[item_id] = get_item_metadata(params={'item': int(item_id), 'specification': ["title", "release_date", "genres"]}, return_dict=True)
+        print("\n" + str(interaction_dict) + "\n")
+        print("\n" + str(response_dict) + "\n")
+        return json.dumps({
+            "status": "success",
+            "message": f"Suggested recommendations for user {user}: {response_dict}. Please, include the movie ID when listing the recommended items. After listing the recommended items, if the user asks for an explanation in a subsequent prompt, please try to provide an explanation for the recommendations based on the similarities between recommended items and the items the user interacted in the past, that are: {interaction_dict}. To explain recommendations, you could also use additional information that you might know. Please, ask the user if he/she would like an explanation to be provided. If the answer is yes, explain the recommendations."
+        })
+    else:
+        return json.dumps({
+            "status": "failure",
+            "message": f"Something went wrong in the function calling. The generated JSON is invalid.",
+        })
+
+
+def get_item_metadata(params, return_dict=False):
+    if 'item' in params and 'specification' in params:
+        conn = sqlite3.connect('movies.db')
+        cursor = conn.cursor()
+        item_id = params.get('item')
+        specification = params.get('specification')
+
+        sql_query = f'SELECT {", ".join(specification)} FROM movies_ml WHERE item_id = {item_id}'
+
+        cursor.execute(sql_query)
+        result = cursor.fetchone()
+
+        conn.close()
+
+        if result and not return_dict:
+            return_str = ""
+            for i, spec in enumerate(specification):
+                return_str += f"{spec}: {result[i]}\n"
+            return json.dumps({
+                "status": "success",
+                "message": f"This is the requested metadata for item {item_id}:\n{return_str}",
+            })
+        elif result and return_dict:
+            r_dict = {}
+            for i, spec in enumerate(specification):
+                r_dict[spec] = result[i]
+            return r_dict
+        else:
+            if not return_dict:
+                return json.dumps({
+                    "status": "failure",
+                    "message": f"No information found for the given item.",
+                })
+            else:
+                return None
+    else:
+        return json.dumps({
+            "status": "failure",
+            "message": f"Something went wrong in the function calling. The generated JSON is invalid.",
+        })
+
 
 def function_call_handler(tool_name, arguments):
     if tool_name == "get_top_k_recommendations":
-        return json.dumps({
-            "status": "success",
-            "message": f"Suggested recommendations for user {arguments.get('user')}: 45, 65, 43"
-        })
-    elif tool_name == "get_item_metadata":
-        return json.dumps({
-            "status": "success",
-            "message": f"Here's the requested metadata ({arguments.get('specification')}) for item {arguments.get('item')}: Oblivion"
-        })
-    raise ValueError("Wrong tool name passed!!")
+        return get_top_k_recommendations(arguments)
+    if tool_name == "get_item_metadata":
+        return get_item_metadata(arguments)
+
+    return json.dumps({
+        "status": "success",
+        "message": f"Executed tool '{tool_name}' successfully."
+    })
+
+
+create_db()
+client = Entity(base_url="http://localhost:9000", api_key=os.getenv("API_KEY"))
+user = client.users.retrieve_user('user_HkTZCaqKmvEOKFzQRfjtLR')
+thread = client.threads.create_thread(participant_ids=[user.id])
+assistant = client.assistants.retrieve_assistant("default")
+associate_tool_to_assistant(assistant.id, tool=RECOMMENDATION)
+associate_tool_to_assistant(assistant.id, tool=METADATA)
+
 
 @on_message
 async def handle_message(message):
@@ -77,18 +273,17 @@ async def handle_message(message):
         assistant_id=assistant.id,
         message_id=message.id,
         run_id=run.id,
-        api_key=os.getenv("HYPERBOLIC_API_KEY")
+        api_key=os.getenv("HYPERBOLIC_API_KEY"),
     )
 
     for chunk in sync_stream.stream_chunks(
         provider="Hyperbolic",
         model="hyperbolic/deepseek-ai/DeepSeek-V3",
-        timeout_per_chunk=15.0
+        timeout_per_chunk=15.0,
+        api_key=os.getenv("HYPERBOLIC_API_KEY"),
     ):
         token = chunk.get("content", "")
         await msg.stream_token(token)
-
-    await msg.update()
 
     try:
         action_was_handled = client.runs.poll_and_execute_action(
@@ -110,7 +305,7 @@ async def handle_message(message):
                 assistant_id=assistant.id,
                 message_id="regenerated",
                 run_id=run.id,
-                api_key=os.getenv("HYPERBOLIC_API_KEY")
+                api_key=os.getenv("HYPERBOLIC_API_KEY"),
             )
             for final_chunk in sync_stream.stream_chunks(
                 provider="Hyperbolic",
@@ -120,7 +315,7 @@ async def handle_message(message):
             ):
                 token = final_chunk.get("content", "")
                 await msg.stream_token(token)
-
-            await msg.update()
     except Exception as e:
         print(f"\n[Error during tool execution or final stream]: {str(e)}")
+
+    await msg.update()
